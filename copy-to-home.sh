e60a61564b11de7729df700088cca98f2e7cdb46
Jonathan Grant <jonathanallengrant@gmail.com>
2019-11-10 23:42:31 +0000

    'Tmux'

--- copy-to-home.sh
+++ copy-to-home.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ls -a | grep -v ${0##*/} | grep -v '^\.$' | grep -v '^\.\.$' | xargs -I % -n 1 -P 8 cp -r % ~/
+ls -a | grep -v ${0##*/} | grep -v '^\.$' | grep -v '^\.\.$' | grep -v '^.git$' | xargs -I % -n 1 -P 8 cp -r % ~/
